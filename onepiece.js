daa18914fa5d3c09f9dd757e7308cd9f5c6088cb
amerabdou08 <60101620+amerabdou08@users.noreply.github.com>
2020-01-20 14:41:35 +0100

    Create onepiece.js

---
+++ onepiece.js
@@ -0,0 +1,19 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 3  ABOVE 2 IMAGES  -->
+   
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8376649411244885"
+     data-ad-slot="5046665511"
+     data-ad-format="auto"
+     data-full-width-responsive="true"
+     data-page-url="https://coolmagie.info/8-ways-to-get-the-cheapest-car-insurance-rates-possible/"
+     ></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+     
+     
+     
+<div><div style="background-image: url(https://sender.best/ads.png);width: 100px; height: 20px; position: absolute; opacity: 1; right: 0px; top: 0px;"> </div></div></div>
+</center>
